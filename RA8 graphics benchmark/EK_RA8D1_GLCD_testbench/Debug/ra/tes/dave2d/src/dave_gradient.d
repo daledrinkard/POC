5407aa5fe7374e9d91e8d2a09a0d85dff130cd7c
Dale Drinkard <dale.drinkard@renesas.com>
2024-08-04 19:12:01 -0400

    what am i doing

--- RA8 graphics benchmark/EK_RA8D1_GLCD_testbench/Debug/ra/tes/dave2d/src/dave_gradient.d
+++ RA8 graphics benchmark/EK_RA8D1_GLCD_testbench/Debug/ra/tes/dave2d/src/dave_gradient.d
@@ -1,29 +1,29 @@
 ra/tes/dave2d/src/dave_gradient.o: ../ra/tes/dave2d/src/dave_gradient.c \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_driver.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_types.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_errorcodes.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_driver.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_types.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_errorcodes.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h \
  ../ra/tes/dave2d/src/dave_intern.h \
  ../ra/tes/dave2d/src/dave_registermap.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h \
  ../ra/tes/dave2d/src/dave_dlist.h ../ra/tes/dave2d/src/dave_rbuffer.h \
  ../ra/tes/dave2d/src/dave_hardware.h ../ra/tes/dave2d/src/dave_context.h \
  ../ra/tes/dave2d/src/dave_gradient.h \
- C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h \
  ../ra/tes/dave2d/src/dave_render.h
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_driver.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_types.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_errorcodes.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_driver.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_types.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_errorcodes.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h:
 ../ra/tes/dave2d/src/dave_intern.h:
 ../ra/tes/dave2d/src/dave_registermap.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_base.h:
 ../ra/tes/dave2d/src/dave_dlist.h:
 ../ra/tes/dave2d/src/dave_rbuffer.h:
 ../ra/tes/dave2d/src/dave_hardware.h:
 ../ra/tes/dave2d/src/dave_context.h:
 ../ra/tes/dave2d/src/dave_gradient.h:
-C:/Users/daled/e2_studio/workspace/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_testbench/ra/tes/dave2d/inc/dave_math.h:
 ../ra/tes/dave2d/src/dave_render.h:
