b824386afe264cfa86e5c79f43d2c1cd47b80eb3
Dale Drinkard <dale.drinkard@renesas.com>
2024-08-06 11:09:48 -0400

    deer

---
+++ RA8 graphics benchmark/EK_RA8D1_GLCD_tb0/Debug/ra/board/ra8d1_ek/board_sdram.d
@@ -0,0 +1,109 @@
+ra/board/ra8d1_ek/board_sdram.o: ../ra/board/ra8d1_ek/board_sdram.c \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/instances/r_ioport.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/bsp_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/fsp_version.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_gen/bsp_clock_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_override.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_mcu_info.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_elc.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_feature.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_feature_gen.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_init.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_sdram.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_leds.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_ethernet_phy.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_exceptions.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_gen/vector_data.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/renesas.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/R7FA8D1BH.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm85.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cachel1_armv7.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_common.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_tfu.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_register_protection.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_irq.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_io.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_group_irq.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_clocks.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_module_stop.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_security.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/fsp_features.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_delay.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_mcu_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/r_ioport_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/bsp_api.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/r_ioport_cfg.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h \
+ ../ra/board/ra8d1_ek/board_sdram.h
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/instances/r_ioport.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/bsp_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/fsp_version.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_gen/bsp_clock_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_override.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_mcu_info.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_elc.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_feature.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/ra8d1/bsp_feature_gen.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_init.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_sdram.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_leds.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/board/ra8d1_ek/board_ethernet_phy.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_exceptions.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_gen/vector_data.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/renesas.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/R7FA8D1BH.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm85.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/arm/CMSIS_5/CMSIS/Core/Include/cachel1_armv7.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/cmsis/Device/RENESAS/Include/system.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_common.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_tfu.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_register_protection.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_irq.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_io.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_group_irq.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_clocks.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_module_stop.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_security.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/fsp_features.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/fsp_common_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_compiler_support.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_delay.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/src/bsp/mcu/all/bsp_mcu_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/r_ioport_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/fsp/inc/api/bsp_api.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/r_ioport_cfg.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h:
+../ra/board/ra8d1_ek/board_sdram.h:
---
+++ RA8 graphics benchmark/EK_RA8D1_GLCD_tb0/Debug/ra/tes/dave2d/src/dave_context.d
@@ -0,0 +1,30 @@
+ra/tes/dave2d/src/dave_context.o: ../ra/tes/dave2d/src/dave_context.c \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_driver.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_types.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_errorcodes.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_base.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_math.h \
+ ../ra/tes/dave2d/src/dave_intern.h \
+ ../ra/tes/dave2d/src/dave_registermap.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_base.h \
+ ../ra/tes/dave2d/src/dave_dlist.h ../ra/tes/dave2d/src/dave_rbuffer.h \
+ ../ra/tes/dave2d/src/dave_hardware.h ../ra/tes/dave2d/src/dave_context.h \
+ ../ra/tes/dave2d/src/dave_gradient.h \
+ C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_math.h \
+ ../ra/tes/dave2d/src/dave_memory.h ../ra/tes/dave2d/src/dave_texture.h
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_driver.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_types.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_errorcodes.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_base.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_math.h:
+../ra/tes/dave2d/src/dave_intern.h:
+../ra/tes/dave2d/src/dave_registermap.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_base.h:
+../ra/tes/dave2d/src/dave_dlist.h:
+../ra/tes/dave2d/src/dave_rbuffer.h:
+../ra/tes/dave2d/src/dave_hardware.h:
+../ra/tes/dave2d/src/dave_context.h:
+../ra/tes/dave2d/src/dave_gradient.h:
+C:/Projects/Projects\ (repos)/RA8\ graphics\ benchmark/EK_RA8D1_GLCD_tb0/ra/tes/dave2d/inc/dave_math.h:
+../ra/tes/dave2d/src/dave_memory.h:
+../ra/tes/dave2d/src/dave_texture.h:
